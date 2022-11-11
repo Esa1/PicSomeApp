dd6483a2a27713bb5a3ade5a2c72e95982dd2447
Esa Torkkila <esa.torkkila@gmail.com>
2022-11-11 08:18:35 +0200

    React router setup

---
+++ src/pages/Photos.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Photos() {
+    console.log("Photos")
+    return (
+        <main className="photos">
+            <h1>Images go here</h1>
+        </main>
+    )
+}
+
+export default Photos
