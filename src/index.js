40e6b54c1cf2bf676caa9b3966aacc96db66fb7a
Esa Torkkila <esa.torkkila@gmail.com>
2022-11-13 11:46:31 +0200

    Context setup

--- src/index.js
+++ src/index.js
@@ -3,28 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"
+import {PicSomeProvider} from "./components/usePicSome"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <App />
-    </Router>
+    <PicSomeProvider>
+        <Router>
+            <App />
+        </Router>
+    </PicSomeProvider>
 );
 
-
-/*
-import React from "react"
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-
-import "./styles.css"
-import App from "./App"
-
-//ReactDOM.render(<App />, document.getElementById("root"))
-const root = createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
-*/
