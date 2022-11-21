64c49c0c25eba6252f862239438f945b32b94546
Esa Torkkila <esa.torkkila@gmail.com>
2022-11-21 02:46:37 +0200

    Display icons on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,24 +2,32 @@ import React from "react"
 import { useState } from "react"
 
 /*
-On the Image component, track the hover state
+Conditionally render the heart and plus icons when an Image component is being hovered on
 
-1. Create state boolean for "hovered"
-2. When the mouse enters the Image's div, set "hovered" to true
-3. When the mouse leaves the Image's div, set "hovered" to false
-4. Log "hovered" to the console so you know it's changing successfully.
+1. Icon to render for the heart:
+<i className="ri-heart-line favorite"></i>
+
+2. Icon to render for the plus:
+<i className="ri-add-circle-line cart"></i>
 */
+
 function Image({img, className}) {
     //console.log('Image: ' + img)
     const [isHovered, setIsHovered] = useState(false)
     console.log(`isHovered=${isHovered}, ID=${img.id}`)
 
-    return (
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    
+   return (
         <div className={`${className} image-container`} 
              onMouseEnter={() => setIsHovered(true)}
              onMouseLeave={() => setIsHovered(false)}
         >
             <img src={img.url} className="image-grid" alt=""/>
+            {heartIcon}
+            {cartIcon}
+
         </div>
     )
 }
