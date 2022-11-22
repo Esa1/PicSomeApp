dda332b4781839bfa6627114597c1e749329180e
Esa Torkkila <esa.torkkila@gmail.com>
2022-11-22 07:07:18 +0200

    Indicate when image is already in the cart

--- src/Context.js
+++ src/Context.js
@@ -1,12 +1,6 @@
 import React, {createContext, useEffect, useState} from "react";
 const Context = createContext()
-/*
-Setup context to manage items in an array called `cartItems`. This will be an array of image objects.
 
-1. Add the `cartItems` state to context. (Array)
-2. Add function to add an image to the cart. (Takes the full image object as parameter)
-3. Make it so clicking the plus icon on the image adds the item to the cart. (Console.log the cart items array to see that it's working)
-*/
 function PicContextProvider({children}) {
     const [photos, setPhotos] = useState([])
     const [cartItems, setCartItems] = useState([])
@@ -38,17 +32,9 @@ function PicContextProvider({children}) {
     function addToCart(newItem) {
         setCartItems(prevItems => [...prevItems, newItem])
     }
-    console.log(cartItems)
-/*    cartItems.forEach(cartItems => {
-        console.log(`id:${cartItems.id}, 
-                    isFavorite:${cartItems.isFavorite},
-                    url:${cartItems.url},`)
-        
-    });*/
-
 
     return (
-        <Context.Provider value={{photos, toggleFavorite, addToCart}}>
+        <Context.Provider value={{photos, toggleFavorite, addToCart, cartItems}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,22 @@
 import React, { useState, useContext } from "react"
 import PropTypes from "prop-types"
 import { Context } from "../Context"
+/*
+# Challenge
 
+Change the plus icon to a full shopping cart icon when an image is already in the cart. This should display whether the image is being hovered or not (like the favorite icon).
+
+Icon to use when item already in cart:
+<i className="ri-shopping-cart-fill cart"></i>
+
+Hints: 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+https://stackoverflow.com/a/8217584
+*/
 function Image({img, className}) {
     const [isHovered, setIsHovered] = useState(false)
-    const {toggleFavorite, addToCart} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems} = useContext(Context)
 
     function heartIcon() {
         if(img.isFavorite) {
@@ -13,10 +25,15 @@ function Image({img, className}) {
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
     }
-        
-    const cartIcon = isHovered && <i className="ri-add-circle-line cart" 
-        onClick={() => addToCart(img)}></i>
     
+    function cartIcon() {
+        const alreadyInCart = cartItems.some(item => item.id === img.id)
+        if(alreadyInCart)
+            return <i className="ri-shopping-cart-fill cart" onClick={() => addToCart(img)}></i>
+        else if (isHovered)
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+    }
+
     return (
         <div className={`${className} image-container`} 
              onMouseEnter={() => setIsHovered(true)}
@@ -24,7 +41,7 @@ function Image({img, className}) {
         >
             <img src={img.url} className="image-grid" alt=""/>
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
 
         </div>
     )
